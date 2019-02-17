6499e6caa3a7d2a2966d8578973a4eb7e168d29d
RakeshCherukuri <rakesh.cherukuri2@gmail.com>
2019-02-17 10:49:58 +0000

    few modification to scripts

--- Question-3/check_sum_node_and_service.sh
+++ Question-3/check_sum_node_and_service.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 
 export REMOTE_CODE=/home/testuser/remotecode/
-export NODE_ROOT=/usr/node/data
+export NODE_ROOT=/home/ubuntu/node/data
 
 # Checksum for checking file integrity
-sha512sum -c code_checksum.sha512 2> sha512errors 1>/dev/null
-if [ -s sha512errors ]
-then
-  echo The following errors where found:
-  more sha512errors
-  rm -f sha512errors
-else
-  echo All files are ok.
-  rm -f sha512errors
-fi
+#cd  $REMOTE_CODE 
+#sha512sum code_base.tar.gz  > code_checksum1.txt
+#sha512sum -c code_checksum1.txt code_checksum.txt 2> sha512errors 1>/dev/null
+#if [ -s sha512errors ]
+#then
+#  echo The following errors where found:
+#  more sha512errors
+#  rm -f sha512errors
+#else
+#  echo All files are ok.
+#  rm -f sha512errors
+#fi
 # Stop node if it is running
 if (systemctl -q is-active mongod)
   then
@@ -22,8 +24,8 @@ else
     echo Service not running
 fi
 # Move the code and extract
-mv remotecode/*.tar.gz $NODE_ROOT
-tar -xvzf $NODE_ROOT/code_base.tar.gz
+#mv $REMOTE_CODE/code_base.tar.gz $NODE_ROOT
+#tar -xf $NODE_ROOT/code_base.tar.gz -C $NODE_ROOT
 # Start node 
 if (systemctl -q is-active mongod)
   then
--- Question-3/fill_empty_fields.py
+++ Question-3/fill_empty_fields.py
@@ -1,9 +1,9 @@
 import os
 import json
 
-with open('testuser/mycode/config.json', 'r') as fr:
+with open('/home/testuser/mycode/config.json', 'r') as fr:
     credentials = json.load(fr)
-with open('testuser/mycode/config.json', 'w') as fw:   
+with open('/home/testuser/mycode/config.json', 'w') as fw:   
     for key, value in credentials.items():
         if value == None or value == '':
             credentials[key] = os.environ[key]
